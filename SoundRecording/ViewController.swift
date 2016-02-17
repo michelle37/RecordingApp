3dbbe154d4a7837d63c59c9d2b2f98aabd4a8e33
Michelle Lee <michelle_2001li@michellesimac.hsd1.ga.comcast.net>
2016-02-16 21:22:27 -0500

    Fixed record coding issues

--- SoundRecording/ViewController.swift
+++ SoundRecording/ViewController.swift
@@ -12,6 +12,7 @@ import AVFoundation
 class ViewController: UIViewController {
     
     var audioRecorder:AVAudioRecorder!
+
     
     @IBOutlet weak var Record: UIButton!
     @IBOutlet weak var Text: UILabel!
@@ -23,8 +24,21 @@ class ViewController: UIViewController {
         
     }
     
-    func record(){
-        
+
+      
+   override func viewWillAppear(animated: Bool) {
+        super.viewDidLoad()
+        Text.hidden = true
+        Stop.hidden = true
+        Record.enabled = true
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func RecordingBtn(sender: UIButton) {
         //set the audio path to store in document directory on your device
         let dirPath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as String
         
@@ -44,21 +58,8 @@ class ViewController: UIViewController {
         audioRecorder.meteringEnabled = true
         audioRecorder.prepareToRecord()
         audioRecorder.record()
-    }
-   override func viewWillAppear(animated: Bool) {
-        super.viewDidLoad()
-        Text.hidden = true
-        Stop.hidden = true
-        Record.enabled = true
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    @IBAction func RecordingBtn(sender: UIButton) {
-        print("this is a print test")
+    
+    
         Text.hidden = false
         Stop.hidden = false
         Record.enabled = false
