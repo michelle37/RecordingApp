407327c55400231384782dc3dfa42940cb0d2076
Michelle Lee <michelle_2001li@michellesimac.hsd1.ga.comcast.net>
2016-02-16 19:30:59 -0500

    My first iOS App initial commit

--- SoundRecording/ViewController.swift
+++ SoundRecording/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Record: UIButton!
+    @IBOutlet weak var Text: UILabel!
+    @IBOutlet weak var Stop: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func RecordingBtn(sender: UIButton) {
+    }
+    
+    @IBAction func StopBtn(sender: UIButton) {
+    }
 
 }
 
